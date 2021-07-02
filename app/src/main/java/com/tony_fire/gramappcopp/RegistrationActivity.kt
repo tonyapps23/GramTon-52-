a8bfc20574b7de3d4551b15d0372b445699dfdbb
antony-chepel <72796562+antony-chepel@users.noreply.github.com>
2021-07-02 08:18:25 +0300

    1.0

---
+++ app/src/main/java/com/tony_fire/gramappcopp/RegistrationActivity.kt
@@ -0,0 +1,136 @@
+package com.tony_fire.gramappcopp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import com.InfoClass
+import com.retrofit.RetrofitInstance
+import com.retrofit.UserInfo
+
+import com.tony_fire.gramappcopp.databinding.ActivityRegistrationBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        userField()
+
+    }
+    private fun userField() {
+        binding.lkdfpe.setOnClickListener {
+            val ed1stName = binding.ed1stName.text.toString().trim()
+            val ed2stName = binding.edSecondName.text.toString().trim()
+            val ed_email = binding.edEmail.text.toString().trim()
+            val ed_tel = binding.edPhone.text.toString().trim()
+            val countrypicker = binding.codePicker.selectedCountryNameCode.toString()
+
+
+
+            if(ed1stName.isEmpty()){
+                binding.ed1stName.error = "Введите имя"
+                binding.ed1stName.requestFocus()
+                return@setOnClickListener
+
+            }
+
+            if(ed2stName.isEmpty()){
+                binding.edSecondName.error = "Введите фамилию"
+                binding.edSecondName.requestFocus()
+                return@setOnClickListener
+
+            }
+
+            if(ed_email.isEmpty()){
+                binding.edEmail.error = "Введите Email"
+                binding.edEmail.requestFocus()
+                return@setOnClickListener
+
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(ed_email).matches()){
+                binding.edEmail.error = "Введите корректный Email"
+                binding.edEmail.requestFocus()
+                return@setOnClickListener
+
+
+            }
+            if(ed_tel.isEmpty()){
+                binding.edPhone.error = "Введите телефон"
+                binding.edPhone.requestFocus()
+                return@setOnClickListener
+
+            }
+            binding.lkdfpe.setBackgroundResource(R.drawable.bg_color_select)
+            binding.lkdfpe.isEnabled = false
+
+            val userinfo = UserInfo(countrypicker,ed_email,ed1stName,"5f9955e25cb3ff75e3cfcce8",ed2stName,ed_tel,"grentonprtfg")
+            val call : Call<InfoClass> = RetrofitInstance.api.signUpUser(userinfo)
+
+            call.enqueue(object : Callback<InfoClass> {
+                override fun onResponse(call: Call<InfoClass>, response: Response<InfoClass>) {
+                    if(response.isSuccessful){
+                        Log.d("Info", "Status: ${response.body()?.status}" )
+                        Log.d("Info", "Pr: ${response.body()?.data?.pr}" )
+                        Log.d("Info", "Redirect: ${response.body()?.data?.redirect}" )
+
+                        when {
+                            response.body()?.data?.pr.equals("success") -> {
+                                val i = Intent(
+                                    this@RegistrationActivity,
+                                    ThanksActivity::class.java
+                                ).apply {
+                                    putExtra("Name", userinfo.first_name)
+                                    putExtra("Second", userinfo.last_name)
+
+                                }
+                                startActivity(i)
+                                binding.lkdfpe.setBackgroundResource(R.drawable.bg_selector)
+                                binding.lkdfpe.isEnabled = true
+                            }
+                            response.body()?.data?.pr.equals("exist") -> {
+                                Toast.makeText(this@RegistrationActivity,"Вы уже зарегистрированы!",
+                                    Toast.LENGTH_LONG).show()
+                                binding.lkdfpe.setBackgroundResource(R.drawable.bg_selector)
+                                binding.lkdfpe.isEnabled = true
+                            }
+                            response.body()?.data?.pr.equals("redirect") -> {
+                                val i = Intent(this@RegistrationActivity,WebViewActivity::class.java).apply {
+                                    putExtra("Url",response.body()?.data?.redirect.toString())
+                                }
+                                startActivity(i)
+                                binding.lkdfpe.setBackgroundResource(R.drawable.bg_selector)
+                                binding.lkdfpe.isEnabled = true
+                            }
+                        }
+
+                    }
+                    else {
+                        Toast.makeText(applicationContext,"Произошла ошибка,попробуйте снова", Toast.LENGTH_SHORT).show()
+                        binding.lkdfpe.setBackgroundResource(R.drawable.bg_selector)
+                        binding.lkdfpe.isEnabled = true
+                        return
+                    }
+
+                }
+
+                override fun onFailure(call: Call<InfoClass>, t: Throwable) {
+                    Toast.makeText(applicationContext,"Проверьте интернет соединение", Toast.LENGTH_SHORT).show()
+                    binding.lkdfpe.setBackgroundResource(R.drawable.bg_selector)
+                    binding.lkdfpe.isEnabled = true
+                }
+
+
+            })
+
+
+
+        }
+
+    }
+}
